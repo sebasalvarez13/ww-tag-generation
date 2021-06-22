4e60d62f76214b41af80da1dccec8ea517d55ea4
sebasalvarez13 <sebastian.alvarezarce@gmail.com>
2021-06-22 15:25:40 -0700

    Define: print_to_script(), script_exists() and create_script()

--- scriptcreator/eng_inf_drive_speeds_script.py
+++ scriptcreator/eng_inf_drive_speeds_script.py
@@ -2,9 +2,9 @@
 
 import csv
 import textwrap
-import glob
+import os.path
+from os import path
 
-#from website_example import first_transmitter, last_transmitter, gate_num, line
 
 class EngInfDriveSpeeds:
     def __init__(self, first_vfd, last_vfd, conveyor_type, line):
@@ -31,7 +31,7 @@ class EngInfDriveSpeeds:
         paragraph = [line1, line2, line3, line4, line5, line6, line7]
 
         for line in paragraph:
-            self.create_script(line)
+            self.print_to_script(line)
 
 
     def drive_speeds(self):
@@ -47,7 +47,7 @@ class EngInfDriveSpeeds:
                     line2 = "M{}{}{}_{}Mode_SpeedRef_SP.Name;".format(self.conveyor_type_letter, i, self.line, j)
 
                     str1 = (line1 + line2)
-                    self.create_script(str1)
+                    self.print_to_script(str1)
    
         elif self.conveyor_type == "Modulation":
             for i in range(self.first_vfd, self.last_vfd + 1):
@@ -56,7 +56,7 @@ class EngInfDriveSpeeds:
                     line2 = "M{}{}{}_{}_SpeedRef_SP.Name;".format(self.conveyor_type_letter, i, self.line, j)
 
                     str1 = (line1 + line2)
-                    self.create_script(str1)
+                    self.print_to_script(str1)
              
         elif self.conveyor_type == "Transfer":
             for i in range(self.first_vfd, self.last_vfd + 1):
@@ -64,7 +64,7 @@ class EngInfDriveSpeeds:
                 line2 = "M{}{}{}_SpeedRef_SP.Name;".format(self.conveyor_type_letter, i, self.line)
 
                 str1 = (line1 + line2)
-                self.create_script(str1)
+                self.print_to_script(str1)
 
 
     def text(self):
@@ -80,7 +80,7 @@ class EngInfDriveSpeeds:
                     line2 = "{} Conveyor M{}{}{} {} Speed.Text;".format(self.conveyor_type, self.conveyor_type_letter, i, self.line, mod_speeds[j])
 
                     str1 = (line1 + line2)
-                    self.create_script(str1)
+                    self.print_to_script(str1)
              
         else:
             for i in range(self.first_vfd, self.last_vfd + 1):
@@ -88,10 +88,10 @@ class EngInfDriveSpeeds:
                 line2 = "{} Conveyor M{}{}{}".format(self.conveyor_type, self.conveyor_type_letter, i, self.line)
 
                 str1 = (line1 + line2)
-                self.create_script(str1)
+                self.print_to_script(str1)
 
 
-    def create_script(self, str1):
+    def print_to_script(self, str1):
         script_file = "window-scripts/script_infeed_drive_speeds.txt"
         try:
             with open(script_file, 'a') as scriptfile:
@@ -99,11 +99,30 @@ class EngInfDriveSpeeds:
                 scriptfile.write(textwrap.dedent("\n"))      
         except IOError as e:
             print(e)
-        
+
+
+    def script_exists(self):
+        file_path = "/mnt/c/Projects/ww-tag-generation/window-scripts/script_infeed_drive_speeds.txt"
+        if path.exists(file_path):
+            return True
+        else:
+            return False  
+
+
+    def create_script(self):
+        if self.script_exists() != True :
+            self.header()
+            self.print_to_script(" ")
+
+        self.drive_speeds()
+        self.print_to_script(" ")
+        self.text()
+        self.print_to_script(" ")                   
+               
 
 if __name__ == "__main__":
     first_vfd = 1
     last_vfd = 3
     wm = EngInfDriveSpeeds(first_vfd, last_vfd, "Transfer", "A")
-    wm.header()
-    wm.drive_speeds()
+    wm.create_script()
+    
