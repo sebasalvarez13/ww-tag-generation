13b0b89f1d19bc75962410f188f14c33391d7b82
sebasalvarez13 <sebastian.alvarezarce@gmail.com>
2021-06-29 10:48:51 -0700

    Imported features() form indirect-analog_features.py

--- tagcreator/indirect/pafa_indirect_analog.py
+++ tagcreator/indirect/pafa_indirect_analog.py
@@ -1,34 +1,17 @@
 #!/usr/bin/env python3
 
 import csv
+from indirect_analog_features import features
 
 class PAFAIndirectAnalog:
     def __init__(self):
         self.number_type = ["Decimal", "Percent"]
         self.outer_list = ["CV", "L", "PV"]
-        
-
-    def features(self):
-        dict_data = []
-        my_dict = {
-            ":IndirectAnalog":"",
-            "Group": "$System",
-            "Comment": "",
-            "Logged": "No",
-            "EventLogged": "No",
-            "EventLoggingPriority": 0,
-            "RetentiveValue": "No",
-            "SymbolicName": ""
-            }
-
-        dict_data.append(my_dict)
-
-        return(my_dict)
 
 
     def bag_speed(self):
         dict_data = []
-        dict1 = self.features()
+        dict1 = features()
         dict1[":IndirectAnalog"] = "GenericPAFABagSpeed"
         dict_data.append(dict1)
 
@@ -37,7 +20,7 @@ class PAFAIndirectAnalog:
 
     def bag_weight(self):
         dict_data = self.bag_speed()
-        dict1 = self.features()
+        dict1 = features()
         dict1[":IndirectAnalog"] = "GenericPAFABagWeight"
         dict_data.append(dict1)
 
@@ -46,7 +29,7 @@ class PAFAIndirectAnalog:
 
     def multiplier(self):
         dict_data = self.bag_weight()
-        dict1 = self.features()
+        dict1 = features()
         dict1[":IndirectAnalog"] = "GenericPAFAMultiplier"
         dict_data.append(dict1)
 
@@ -55,7 +38,7 @@ class PAFAIndirectAnalog:
 
     def pid(self):
         dict_data = self.multiplier()
-        dict1 = self.features()
+        dict1 = features()
         dict1[":IndirectAnalog"] = "GenericPAFAPidSp"
         dict_data.append(dict1)
 
@@ -64,7 +47,7 @@ class PAFAIndirectAnalog:
 
     def wf_trend(self):
         dict_data = self.pid()
-        dict1 = self.features()
+        dict1 = features()
         dict1[":IndirectAnalog"] = "GenericPAFAWFTrend"
         dict_data.append(dict1)
 
@@ -74,7 +57,7 @@ class PAFAIndirectAnalog:
     def bpm(self):
         dict_data = self.wf_trend()
         for number in self.number_type:
-            dict1 = self.features()
+            dict1 = features()
             dict1[":IndirectAnalog"] = "GenericPAFABpmActual{}".format(number)
             dict_data.append(dict1)
 
@@ -84,7 +67,7 @@ class PAFAIndirectAnalog:
     def duty_cycle(self):
         dict_data = self.bpm()
         for number in self.number_type:
-            dict1 = self.features()
+            dict1 = features()
             dict1[":IndirectAnalog"] = "GenericPAFADutyCycle{}".format(number)
             dict_data.append(dict1)
 
@@ -94,7 +77,7 @@ class PAFAIndirectAnalog:
     def outer(self):
         dict_data = self.duty_cycle()
         for outer in self.outer_list:
-            dict1 = self.features()
+            dict1 = features()
             dict1[":IndirectAnalog"] = "GenericPAFAOuter{}".format(outer)
             dict_data.append(dict1)
 
