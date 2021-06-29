37a959e9e0ccc46e098057589a330e2290ba1801
sebasalvarez13 <sebastian.alvarezarce@gmail.com>
2021-06-29 16:03:50 -0700

    Fixed features() import line

--- tagcreator/indirect/wm_indirect_analog.py
+++ tagcreator/indirect/wm_indirect_analog.py
@@ -3,6 +3,8 @@
 import csv
 import os.path
 from os import path
+from tagcreator.indirect.indirect_analog_features import features
+
 
 
 class WMIndirectAnalog:
@@ -15,36 +17,10 @@ class WMIndirectAnalog:
     
         self.line = line
 
-    
-    def features(self):
-        dict_data = []
-        my_dict = {
-            ":IndirectAnalog":"",
-            "Group": "$System",
-            "Comment": "",
-            "Logged": "No",
-            "EventLogged": "No",
-            "EventLoggingPriority": 0,
-            "RetentiveValue": "No",
-            "SymbolicName": ""
-            }
-
-        dict_data.append(my_dict)
-
-        return(my_dict)
-
-    def drives(self):
-        dict_data = []
-        dict1 = self.features()
-        dict1[":IndirectAnalog"] = "Generic{}{}CSW".format(self.conveyor_type_short, self.line)            
-        dict_data.append(dict1)
-
-        return(dict_data) 
-
 
     def bag_speed(self):
-        dict_data = self.drives()
-        dict1 = self.features()
+        dict_data = []
+        dict1 = features()
         dict1[":IndirectAnalog"] = "GenericBagSpeedEntry"
         dict_data.append(dict1)
 
@@ -53,7 +29,7 @@ class WMIndirectAnalog:
 
     def bag_weight(self):
         dict_data = self.bag_speed()
-        dict1 = self.features()
+        dict1 = features()
         dict1[":IndirectAnalog"] = "GenericBagWeightEntry"
         dict_data.append(dict1)
 
@@ -62,7 +38,7 @@ class WMIndirectAnalog:
 
     def control(self):
         dict_data = self.bag_weight()
-        dict1 = self.features()
+        dict1 = features()
         dict1[":IndirectAnalog"] = "GenericControlCSW"           
         dict_data.append(dict1)
 
@@ -71,7 +47,7 @@ class WMIndirectAnalog:
 
     def control_disable(self):
         dict_data = self.control()
-        dict1 = self.features()
+        dict1 = features()
         dict1[":IndirectAnalog"] = "GenericControlDisableCSW"           
         dict_data.append(dict1)
 
@@ -80,7 +56,7 @@ class WMIndirectAnalog:
 
     def suspension_timer(self):
         dict_data = self.control_disable()
-        dict1 = self.features()
+        dict1 = features()
         dict1[":IndirectAnalog"] = "GenericEngWmSp_SuspensionTimer"           
         dict_data.append(dict1)
 
@@ -88,7 +64,7 @@ class WMIndirectAnalog:
 
     def line_select(self):
         dict_data = self.suspension_timer()
-        dict1 = self.features()
+        dict1 = features()
         dict1[":IndirectAnalog"] = "GenericLineSelect"           
         dict_data.append(dict1)
 
@@ -97,7 +73,7 @@ class WMIndirectAnalog:
 
     def weigher_control(self):
         dict_data = self.line_select()
-        dict1 = self.features()
+        dict1 = features()
         dict1[":IndirectAnalog"] = "GenericWeigherControlPkg"           
         dict_data.append(dict1)
 
@@ -113,22 +89,28 @@ class WMIndirectAnalog:
 
 
     def create_csv(self):
-        csv_file = "csv-files/indirect/wm_indirect_analog.csv"
-        if self.module_exists() != True :        
-            dict_data = self.weigher_control()
-        else:
-            dict_data = self.drives()     
+        csv_file = "csv-files/indirect/wm_indirect_analog.csv"       
+        dict_data = self.weigher_control()
         csv_columns = list(dict_data[0].keys())
-        
-        try:
-            with open(csv_file, 'a') as csvfile:
-                writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
-                if self.module_exists() != True:
+
+        if self.module_exists() != True:
+            try:
+                with open(csv_file, 'w') as csvfile:
+                    writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
                     writer.writeheader()
-                for data in dict_data:
-                    writer.writerow(data)
-        except IOError as e:
-            print(e)              
+                    for data in dict_data:
+                        writer.writerow(data)
+            except IOError as e:
+                print(e)
+        else:
+            try:
+                with open(csv_file, 'a') as csvfile:
+                    writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
+                    #writer.writeheader()
+                    for data in dict_data:
+                        writer.writerow(data)
+            except IOError as e:
+                print(e)                     
 
 if __name__ == "__main__":
     wm = WMIndirectAnalog("Distribution", 'A')
