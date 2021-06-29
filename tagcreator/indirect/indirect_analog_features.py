6c92b1d98a4e4f4824be75004aa3430d2923e8dc
sebasalvarez13 <sebastian.alvarezarce@gmail.com>
2021-06-29 11:19:50 -0700

    Returns the headers for indirect analog tags for all modules

---
+++ tagcreator/indirect/indirect_analog_features.py
@@ -0,0 +1,17 @@
+
+
+def features():
+    dict_data = []
+    my_dict = {
+        ":IndirectAnalog":"",
+        "Group": "$System",
+        "Comment": "",
+        "EventLogged": "No",
+        "EventLoggingPriority": 0,
+        "RetentiveValue": "No",
+        "SymbolicName": ""
+    }
+
+    dict_data.append(my_dict)
+
+    return(my_dict)
